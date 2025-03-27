5b623475d9034f50f5b0b6db99ca4dc20a6ff18e
Dom Mitchell <domenickmitchell00@gmail.com>
2025-03-27 00:12:23 -0400

    Finished Explorer Mode

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using Microsoft.VisualBasic;
 
 namespace AllCardsOnDeckCSRemake
 {
@@ -6,7 +8,35 @@ namespace AllCardsOnDeckCSRemake
   {
     static void Main(string[] args)
     {
-      Console.WriteLine("Welcome to C#");
+      var deck = new List<string>() { };
+      var suits = new List<string>() { "Hearts", "Diamonds", "Clubs", "Spades" };
+      var ranks = new List<string>() { "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King", "Ace" };
+
+      // Create a deck of cards
+      foreach (var suit in suits)
+      {
+        foreach (var rank in ranks)
+        {
+          deck.Add(rank + " of " + suit);
+        }
+      }
+
+      // Shuffle the deck
+      var random = new Random();
+      for (int i = 0; i < deck.Count; i++)
+      {
+        int j = random.Next(i, deck.Count);
+        var temp = deck[i];
+        deck[i] = deck[j];
+        deck[j] = temp;
+      }
+
+      // Display top 2 cards
+      Console.WriteLine("Top 2 cards:");
+      for (int i = 0; i < 2; i++)
+      {
+        Console.WriteLine(deck[i]);
+      }
     }
   }
 }
